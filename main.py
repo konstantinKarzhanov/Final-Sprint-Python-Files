fc74df5b9515f4d385eeb5ab92670952c4c2608d
Matthew Davis <86232146+Mattdavis80@users.noreply.github.com>
2023-04-07 14:50:30 -0230

    Changes to Python File, Added FormatValues.py

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 # Semester 1 - Final Sprint Week - Simpson Carpet World Python Program
 # Group 3 - Michael Bennett, Matt Davis, Evan Holloway, Kostiantyn Karzhanov, Daniel Shepelev, Michael Sheridan
-# Last Modified - April 4th, 2023
+# Last Modified - April 6th, 2023
 
 # Import Statements
 import re
 import datetime
+import FormatValues as FV
 
 # Variables for Today
-today = datetime.datetime.now() 
-today_str = datetime.datetime.strftime(today, "%m/%d/%Y")
+today = datetime.datetime.now()
+today_str = datetime.datetime.strftime(today, "%d-%m-%Y")
 
-# Lists for validation and receipts 
+# Lists for validation and receipts
 valid_prov = ["NL", "PE", "NS", "NB", "QC", "ON", "MB", "SK", "AB", "BC", "YT", "NT", "NU"]
 branch_list = ["St. John's", "Mt. Pearl", "Carbonear", "Northern Bay"]
 
-# Read data from the file defaults.dat ----- Constants should be all uppercase ----
+# Read data from the file defaults.dat
 f = open('defaults.dat', 'r')
 employee_num = int(f.readline())
 inventory_num = int(f.readline())
@@ -24,15 +25,73 @@ commission_bonus_amt = int(f.readline())
 reorder_num = int(f.readline())
 customer_num = int(f.readline())
 order_num = int(f.readline())
-HST_RATE = float(f.readline())
+HST = float(f.readline())
 f.close()
 
+# A number of lists that will help with validating input in option_four() function
+item_numbers_list = []
+item_descriptions_list = []
+emp_numbers_list = []
+customer_numbers_list = []
+
+# Read required data from the inventory file
+f = open('inventoryLog.dat', 'r')
+for item_data_line in f:
+    item_line = item_data_line.split(',')
+    item_number = int(item_line[0].strip())
+    item_description = item_line[1].strip()
+    retail_price = item_line[7].strip()
+
+    item_numbers_list.append(item_number)
+    item_descriptions_list.append(item_description)
+f.close()
+
+# Read required data from employee file
+f = open("employeeLog.dat", "r")
+for employee_data_line in f:
+    employee_line = employee_data_line.split(',')
+    employee_num = employee_line[0].strip()
+
+
+    emp_numbers_list.append(employee_num)
+f.close()
+
+# Read required data from customer file
+f = open("customerLog.dat", "r")
+for customer_data_line in f:
+    customer_line = customer_data_line.split(',')
+    customer_number = int(customer_line[0].strip())
+
+    customer_numbers_list.append(customer_number)
+f.close()
+
+# Read required data from purchases file
+f = open("customerPurchase.dat", "r")
+
+for purchase_data_line in f:
+    purchase_line = purchase_data_line.split(',')
+    order_number = int(purchase_line[0].strip())
+f.close()
+
+print(employee_num)
+print(inventory_num)
+print(commission_rate)
+print(bonus_threshold)
+print(commission_bonus_amt)
+print(reorder_num)
+print(customer_num)
+print(order_num)
+print(HST)
+
+
 def option_one():
+    """A function that will enter a new employee's information, then
+       append it all into the employee file as a new record."""
     global employee_num
-    # User Validated Inputs
-    # First name, mandatory input, alpha only, converted to Title-case
     employee_info = []
 
+    # User Validated Inputs
+    # First name, mandatory input, alpha only, converted to Title-case
     while True:
         emp_f_name = input("First Name: ").title()
         if emp_f_name == "":
@@ -76,7 +135,7 @@ def option_one():
             print("Please re-enter province as (XX)")
         elif prov == "":
             print("Province field cannot be empty, Please re-enter")
-        elif not prov in valid_prov:
+        elif prov not in valid_prov:
             print("Please enter a valid province")
         else:
             break
@@ -111,7 +170,7 @@ def option_one():
         try:
             date_hired = input("Please enter date hired as dd-mm-yyyy: ")
             date_hired = datetime.datetime.strptime(date_hired, "%d-%m-%Y")
-        except:
+        except TypeError:
             print("Please enter a valid date: ")
         else:
             date_hired = datetime.datetime.strftime(date_hired, "%d-%m-%Y")
@@ -121,7 +180,7 @@ def option_one():
     while True:
         try:
             emp_branch_num = int(input("Please enter branch number (0-3): "))
-        except:
+        except ValueError:
             print("Please enter valid number")
         else:
             if emp_branch_num < 0 or emp_branch_num > 3:
@@ -130,9 +189,9 @@ def option_one():
                 break
 
     # Employee title, mandatory input, checked via regular expressions, Alpha only 20 character max size.
-    pattern =r"^[a-zA-Z ]{0,20}$"
+    pattern = r"^[a-zA-Z ]{0,20}$"
     while True:
-        emp_title = input("Employee Title: "). title()
+        emp_title = input("Employee Title: ").title()
         if re.match(pattern, emp_title):
             break
         else:
@@ -142,7 +201,7 @@ def option_one():
     while True:
         try:
             emp_salary = int(input("Employee Salary: "))
-        except:
+        except ValueError:
             print("Please enter a valid number")
         else:
             if emp_salary < 13000:
@@ -151,7 +210,7 @@ def option_one():
                 print("This salary is above the highest paid person at Sampson's Carpet, Please re-enter")
             else:
                 break
-    
+
     # Employee Skills, mandatory input, alpha only
     while True:
         emp_skills = input("Enter employee skill: ").title()
@@ -165,24 +224,24 @@ def option_one():
         try:
             birthdate = input("Please enter birthdate as dd-mm-yyyy: ")
             birthdate = datetime.datetime.strptime(birthdate, "%d-%m-%Y")
-        except:
+        except TypeError:
             print("Please enter a valid date: ")
         else:
             birthdate = datetime.datetime.strftime(birthdate, "%d-%m-%Y")
             break
-    
-    # Add all info to employee info list
-    employee_info.append((employee_num, emp_f_name, emp_l_name, str_add, city, prov, post_code, phone_num, date_hired, emp_branch_num, emp_title, emp_salary, emp_skills, birthdate))
+
+    # Increment employee_num and add all info to employee info list
+    employee_num += 1
+    employee_info.append((employee_num, emp_f_name, emp_l_name, str_add, city, prov, post_code, phone_num, date_hired,
+                          emp_branch_num, emp_title, emp_salary, emp_skills, birthdate))
 
     # Append info to Employee Log data file
     f = open('employeeLog.dat', 'a')
-    for data in employee_info:
-        f.write(",".join(map(str, data)) + "\n")
+    for data in f:
+        f.write(", ".join(map(str, data)) + "\n")
     f.close()
 
-    employee_num += 1
     # Updating defaults file to new Employee Number
-    employee_info[0] += 1
     f = open("defaults.dat", 'w')
     f.write("{}\n".format(str(employee_num)))
     f.write("{}\n".format(str(inventory_num)))
@@ -193,7 +252,7 @@ def option_one():
     f.write("{}\n".format(str(customer_num)))
     f.write("{}\n".format(str(order_num)))
     f.write("{}\n".format(str(HST)))
-    f.close
+    f.close()
 
 
 def option_two():
@@ -205,10 +264,130 @@ def option_three():
 
 
 def option_four():
+    """A function that is used to enter information for each new customer
+       purchase, then append all the info into the customer purchase file
+       as a new record."""
+
+    global order_number
+    purchase_info = []
+
+    print("Enter new order information")
+    while True:
+        emp_number = input("Employee Number: ")
+        if emp_number not in emp_numbers_list:
+            print("Does not match any current employee number - please try again.")
+            print(f"Employee numbers: {emp_numbers_list}")
+        else:
+            break
+
+    while True:
+        try:
+            cust_num = int(input("Customer ID Number: "))
+        except ValueError:
+            print("Invalid input - must be an integer.")
+        else:
+            if int(cust_num) not in customer_numbers_list:
+                print("Invalid customer number - please try again.")
+                print(f"Customer Numbers: {customer_numbers_list}")
+            else:
+                break
+
+    while True:
+        try:
+            item_num = int(input("Item Number: "))
+        except ValueError:
+            print("Invalid input - must be an integer.")
+        else:
+            if int(item_num) not in item_numbers_list:
+                print("That item number does not match any of our items - please try again.")
+                print(f"Item Numbers: {item_numbers_list}")
+            else:
+                break
+
+    while True:
+        description = input("Item Description: ").title()
+        if description == "":
+            print("Cannot be blank - please try again.")
+        elif description not in item_descriptions_list:
+            print("We do not sell any items of that description - please try again.")
+            print(f"Item Descriptions: 'Wool Carpet', 'Nylon Carpet', 'Blended Carpet'")
+        else:
+            break
+
+    while True:
+        try:
+            retail_cost = float(input("Enter the retail cost: "))
+        except ValueError:
+            print("Invalid number - please try again")
+        else:
+            break
+
+    while True:
+        try:
+            quantity = int(input("Enter the quantity purchased: "))
+        except ValueError:
+            print("Invalid number - please try again")
+        else:
+            if quantity < 1:
+                print("Invalid quantity - please try again")
+            else:
+                break
+
+    # Calculations for customer purchase
+    subtotal = retail_cost * quantity
+    HST_amount = subtotal * HST
+    order_total = subtotal + HST_amount
+    order_date = today_str
+
+    #  Increment order_number and put all purchase info into a list
+    order_number += 1
+    purchase_info.append((order_number, order_date, cust_num, item_num, description, retail_cost, quantity, subtotal, order_total, emp_number))
+
+    # Open file and write back all data
+    f = open("customerPurchase.dat", "a")
+    for data in purchase_info:
+        f.write(", ".join(map(str, data)) + "\n")
+    f.close()
     pass
 
 
 def option_five():
+
+    print()
+    print("Simpson's Carpet World")
+    print(f"Employee Listing as of {today_str}")
+    print("------------------------------------------------------------------------------")
+    print("Employee #    Employee Name          Title                  Branch      Salary")
+    print("------------------------------------------------------------------------------")
+
+    total_employees_acc = 0
+    average_salary = 0
+    total_salary_acc = 0
+
+    f = open("employeeLog.dat", "r")
+    for emp_data_line in f:
+        emp_line = emp_data_line.split(",")
+
+        employee_number = int(emp_line[0].strip())
+        emp_first_name = emp_line[1].strip()
+        emp_last_name = emp_line[2].strip()
+        emp_branch = emp_line[9].strip()
+        emp_title = emp_line[10].strip()
+        emp_salary = float(emp_line[11].strip())
+        full_name = emp_first_name + " " + emp_last_name
+
+        print(f"{employee_number}          {full_name:<20s}   {emp_title:<16s}    {emp_branch:>4s}       "
+              f"{FV.FDollar2(emp_salary):>10s}")
+
+        total_employees_acc += 1
+        total_salary_acc += emp_salary
+        average_salary = total_salary_acc / total_employees_acc
+    f.close()
+
+    print("------------------------------------------------------------------------------")
+    print(f"Total Employees: {total_employees_acc:>3d}    Average Salary: {FV.FDollar2(average_salary):>10s}   "
+          f"Total Salary: {FV.FDollar2(total_salary_acc):>11s}")
+
     pass
 
 
@@ -223,9 +402,10 @@ def option_seven():
 def option_eight():
     pass
 
-while True:
-    # Allow user to enter as many employees as needed, option to escape loop at end via input statement
 
+while True:
+
+    # Allow user to enter as many employees as needed, option to escape loop at end via input statement
     print()
     print("   Simpson Carpet World")
     print("  Company Services System")
@@ -245,7 +425,7 @@ while True:
     while True:
         try:
             choice = int(input("Please make a list selection: "))
-        except:
+        except ValueError:
             print("Please enter a valid number")
         else:
             if choice < 1 or choice > 9:
