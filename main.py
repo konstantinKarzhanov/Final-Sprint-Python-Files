f9a1cb7dc8fa6470678c50ab293efd9c13ecb515
Matthew Davis <86232146+Mattdavis80@users.noreply.github.com>
2023-04-10 08:22:19 -0230

    Added loading message to option_one in main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 import re
 import datetime
 import FormatValues as FV
+import time
 
 # Variables for Today
 today = datetime.datetime.now()
@@ -77,173 +78,197 @@ f.close()
 
 def option_one():
     """A function that will enter a new employee's information, then
-       append it all into the employee file as a new record."""
-    global employee_num
-    employee_info = []
-
-    # User Validated Inputs
-    # First name, mandatory input, alpha only, converted to Title-case
-    while True:
-        emp_f_name = input("First Name: ").title()
-        if emp_f_name == "":
-            print("First name cannot be empty, Please re-enter")
-        elif not emp_f_name.isalpha():
-            print("Please enter a valid name")
-        else:
-            break
-
-    # Last name, mandatory input, alpha only, converted to Title-case
-    while True:
-        emp_l_name = input("Last Name: ").title()
-        if emp_l_name == "":
-            print("Last name cannot be empty, Please re-enter")
-        elif not emp_l_name.isalpha():
-            print("Please enter a valid name")
-        else:
-            break
-
-    # Street Address, mandatory input, converted to Title-case
-    while True:
-        str_add = input("Street Address: ").title()
-        if str_add == "":
-            print("Street address cannot be empty, Please re-enter")
-        else:
-            break
-
-    # City, mandatory input, converted to Title-case
-    while True:
-        city = input("City: ").title()
-        if city == "":
-            print("City cannot be empty, Please re-enter")
-        else:
-            break
-
-    # Province, mandatory input,converted to Upper-case, compared to valid list of provinces
-    while True:
-        prov = input("Province (XX): ").upper()
-
-        if len(prov) != 2:
-            print("Please re-enter province as (XX)")
-        elif prov == "":
-            print("Province field cannot be empty, Please re-enter")
-        elif prov not in valid_prov:
-            print("Please enter a valid province")
-        else:
-            break
-
-    # Postal Code, mandatory input, must be valid format as X0X 0X0
-    # The pattern below is used to compare against user input, this will be done using Regular Expressions
-    # The pattern will accept a space or dash between postal code
-    pattern = r"^[A-Z]\d[A-Z][ -]?\d[A-Z]\d$"
+    append it all into the employee file as a new record."""
 
     while True:
-        post_code = input("Postal Code: (e.g. A1A 1A1):   ").upper()
-        if re.match(pattern, post_code):
-            break
-        else:
-            print("Invalid postal code. Please re-enter")
+        global employee_num
+        employee_info = []
 
-    # Phone Number, mandatory input, 10 characters long
-    while True:
-        phone_num = input("Phone number (Without Spaces): ")
-        phone_num = phone_num.replace("-", "")
-        phone_num = phone_num.replace("/", "")
-        if len(phone_num) != 10:
-            print("Please format phone number as 10 digits without spaces")
-        elif not phone_num.isdigit():
-            print("Please enter a valid phone number")
-        else:
-            phone_num = phone_num[:3] + "-" + phone_num[3:6] + "-" + phone_num[6:]
-            break
-
-    # Date hired, mandatory input, valid date
-    while True:
-        try:
-            date_hired = input("Please enter date hired as dd-mm-yyyy: ")
-            date_hired = datetime.datetime.strptime(date_hired, "%d-%m-%Y")
-        except:
-            print("Please enter a valid date: ")
-        else:
-            date_hired = datetime.datetime.strftime(date_hired, "%d-%m-%Y")
-            break
-
-    # Employee Branch Number, mandatory input
-    while True:
-        try:
-            emp_branch_num = int(input("Please enter branch number (0-3): "))
-        except ValueError:
-            print("Please enter valid number")
-        else:
-            if emp_branch_num < 0 or emp_branch_num > 3:
-                print("Please enter a valid branch number")
+        # User Validated Inputs
+        # First name, mandatory input, alpha only, converted to Title-case
+        while True:
+            emp_f_name = input("First Name: ").title()
+            if emp_f_name == "":
+                print("First name cannot be empty, Please re-enter")
+            elif not emp_f_name.isalpha():
+                print("Please enter a valid name")
             else:
                 break
 
-    # Employee title, mandatory input, checked via regular expressions, Alpha only 20 character max size.
-    pattern = r"^[a-zA-Z ]{0,20}$"
-    while True:
-        emp_title = input("Employee Title: ").title()
-        if re.match(pattern, emp_title):
-            break
-        else:
-            print("Invalid Title, Please re-enter")
-
-    # Employee Salary, mandatory input
-    while True:
-        try:
-            emp_salary = int(input("Employee Salary: "))
-        except ValueError:
-            print("Please enter a valid number")
-        else:
-            if emp_salary < 13000:
-                print("This salary is below minimum wage, please re-enter")
-            elif emp_salary > 100000:
-                print("This salary is above the highest paid person at Sampson's Carpet, Please re-enter")
+        # Last name, mandatory input, alpha only, converted to Title-case
+        while True:
+            emp_l_name = input("Last Name: ").title()
+            if emp_l_name == "":
+                print("Last name cannot be empty, Please re-enter")
+            elif not emp_l_name.isalpha():
+                print("Please enter a valid name")
             else:
                 break
 
-    # Employee Skills, mandatory input, alpha only
-    while True:
-        emp_skills = input("Employee skill: ").title()
-        if not emp_skills.isalpha():
-            print("Please enter valid skill")
-        else:
-            break
+        # Street Address, mandatory input, converted to Title-case
+        while True:
+            str_add = input("Street Address: ").title()
+            if str_add == "":
+                print("Street address cannot be empty, Please re-enter")
+            else:
+                break
 
-    # Birthdate, mandatory input, valid date
-    while True:
-        try:
-            birthdate = input("Please enter birthdate as dd-mm-yyyy: ")
-            birthdate = datetime.datetime.strptime(birthdate, "%d-%m-%Y")
-        except:
-            print("Please enter a valid date: ")
-        else:
-            birthdate = datetime.datetime.strftime(birthdate, "%d-%m-%Y")
-            break
+        # City, mandatory input, converted to Title-case
+        while True:
+            city = input("City: ").title()
+            if city == "":
+                print("City cannot be empty, Please re-enter")
+            else:
+                break
 
-    # Increment employee_num and add all info to employee info list
-    employee_num += 1
-    employee_info.append((employee_num, emp_f_name, emp_l_name, str_add, city, prov, post_code, phone_num, date_hired,
-                          emp_branch_num, emp_title, emp_salary, emp_skills, birthdate))
+        # Province, mandatory input,converted to Upper-case, compared to valid list of provinces
+        while True:
+            prov = input("Province (XX): ").upper()
 
-    # Append info to Employee Log data file
-    f = open('employeeLog.dat', 'a')
-    for data in employee_info:
-        f.write(", ".join(map(str, data)) + "\n")
-    f.close()
+            if len(prov) != 2:
+                print("Please re-enter province as (XX)")
+            elif prov == "":
+                print("Province field cannot be empty, Please re-enter")
+            elif prov not in valid_prov:
+                print("Please enter a valid province")
+            else:
+                break
 
-    # Updating defaults file to new Employee Number
-    f = open("defaults.dat", 'w')
-    f.write("{}\n".format(str(employee_num)))
-    f.write("{}\n".format(str(inventory_num)))
-    f.write("{}\n".format(str(commission_rate)))
-    f.write("{}\n".format(str(bonus_threshold)))
-    f.write("{}\n".format(str(commission_bonus_amt)))
-    f.write("{}\n".format(str(reorder_num)))
-    f.write("{}\n".format(str(customer_num)))
-    f.write("{}\n".format(str(order_num)))
-    f.write("{}\n".format(str(HST)))
-    f.close()
+        # Postal Code, mandatory input, must be valid format as X0X 0X0
+        # The pattern below is used to compare against user input, this will be done using Regular Expressions
+        # The pattern will accept a space or dash between postal code
+        pattern = r"^[A-Z]\d[A-Z][ -]?\d[A-Z]\d$"
+
+        while True:
+            post_code = input("Postal Code: (e.g. A1A 1A1):   ").upper()
+            if re.match(pattern, post_code):
+                break
+            else:
+                print("Invalid postal code. Please re-enter")
+
+        # Phone Number, mandatory input, 10 characters long
+        while True:
+            phone_num = input("Phone number (Without Spaces): ")
+            phone_num = phone_num.replace("-", "")
+            phone_num = phone_num.replace("/", "")
+            if len(phone_num) != 10:
+                print("Please format phone number as 10 digits without spaces")
+            elif not phone_num.isdigit():
+                print("Please enter a valid phone number")
+            else:
+                phone_num = phone_num[:3] + "-" + phone_num[3:6] + "-" + phone_num[6:]
+                break
+
+        # Date hired, mandatory input, valid date
+        while True:
+            try:
+                date_hired = input("Please enter date hired as dd-mm-yyyy: ")
+                date_hired = datetime.datetime.strptime(date_hired, "%d-%m-%Y")
+            except:
+                print("Please enter a valid date: ")
+            else:
+                date_hired = datetime.datetime.strftime(date_hired, "%d-%m-%Y")
+                break
+
+        # Employee Branch Number, mandatory input
+        while True:
+            try:
+                emp_branch_num = int(input("Please enter branch number (0-3): "))
+            except ValueError:
+                print("Please enter valid number")
+            else:
+                if emp_branch_num < 0 or emp_branch_num > 3:
+                    print("Please enter a valid branch number")
+                else:
+                    break
+
+        # Employee title, mandatory input, checked via regular expressions, Alpha only 20 character max size.
+        pattern = r"^[a-zA-Z ]{0,20}$"
+        while True:
+            emp_title = input("Employee Title: ").title()
+            if re.match(pattern, emp_title):
+                break
+            else:
+                print("Invalid Title, Please re-enter")
+
+        # Employee Salary, mandatory input
+        while True:
+            try:
+                emp_salary = int(input("Employee Salary: "))
+            except ValueError:
+                print("Please enter a valid number")
+            else:
+                if emp_salary < 13000:
+                    print("This salary is below minimum wage, please re-enter")
+                elif emp_salary > 100000:
+                    print("This salary is above the highest paid person at Sampson's Carpet, Please re-enter")
+                else:
+                    break
+
+        # Employee Skills, mandatory input, alpha only
+        while True:
+            emp_skills = input("Employee skill: ").title()
+            if not emp_skills.isalpha():
+                print("Please enter valid skill")
+            else:
+                break
+
+        # Birthdate, mandatory input, valid date
+        while True:
+            try:
+                birthdate = input("Please enter birthdate as dd-mm-yyyy: ")
+                birthdate = datetime.datetime.strptime(birthdate, "%d-%m-%Y")
+            except:
+                print("Please enter a valid date: ")
+            else:
+                birthdate = datetime.datetime.strftime(birthdate, "%d-%m-%Y")
+                break
+
+        # Increment employee_num and add all info to employee info list
+        employee_num += 1
+        employee_info.append((employee_num, emp_f_name, emp_l_name, str_add, city, prov, post_code, phone_num, date_hired,
+                            emp_branch_num, emp_title, emp_salary, emp_skills, birthdate))
+
+        # Append info to Employee Log data file
+        f = open('employeeLog.dat', 'a')
+        for data in employee_info:
+            f.write(", ".join(map(str, data)) + "\n")
+        f.close()
+
+        # Updating defaults file to new Employee Number
+        f = open("defaults.dat", 'w')
+        f.write("{}\n".format(str(employee_num)))
+        f.write("{}\n".format(str(inventory_num)))
+        f.write("{}\n".format(str(commission_rate)))
+        f.write("{}\n".format(str(bonus_threshold)))
+        f.write("{}\n".format(str(commission_bonus_amt)))
+        f.write("{}\n".format(str(reorder_num)))
+        f.write("{}\n".format(str(customer_num)))
+        f.write("{}\n".format(str(order_num)))
+        f.write("{}\n".format(str(HST)))
+        f.close()
+
+
+        # Loading text for recept generation
+        print()
+        print("Adding Employee to System, please wait")
+        loading_text = "...."
+
+        for char in loading_text:
+            print(char, end="", flush=True)
+            time.sleep(0.5)
+        print(f" Employee {employee_num} successfully added. ")
+        time.sleep(1)
+
+        while True:
+            add_more_emp = input("Would you like to add another employee (Y/N): ")
+            if add_more_emp == "Y":
+                break
+            elif add_more_emp == "N":
+                return
+            else:
+                print("Please Select (Y) for yes or (N) for no")
+
 
 
 def option_two():
@@ -431,6 +456,7 @@ while True:
         print()
         print('Welcome to the Employee Portal')
         print("Please enter the following information:")
+        print()
         option_one()
     elif choice == 2:
         print()
